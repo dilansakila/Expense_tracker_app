5cbfd4df4f39fb58fa2b366b80230491f989d7e4
dilansakila <dilansakila99@gmail.com>
2024-06-13 13:25:13 +0530

    updated js

--- tracker.js
+++ tracker.js
@@ -1,10 +1,10 @@
-const balance = document.getElementById('balance);
+const balance = document.getElementById('balance');
 const money_plus = document.getElementById('money-plus');
 const money_minus = document.getElementById('money-minus');
-const list = document.getElemntById('list');
+const list = document.getElementById('list');
 const form = document.getElementById('form');
 const text = document.getElementById('text');
-const amount = document.getElementById('');
+const amount = document.getElementById('amount');
 
 const localStorageTransactions = JSON.parse(
     localStorage.getItem('transactions')
